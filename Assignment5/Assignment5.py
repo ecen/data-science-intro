60fecb11d2d4de549eb887d6859a9c934ec5c64b
Davíð Freyr Björnsson <david24099@gmail.com>
2019-10-01 09:27:31 +0200

    Rename.

---
+++ Assignment5/Assignment5.py
@@ -0,0 +1,16 @@
+#%% md
+# # Assignment 5
+# ## Students:
+- Eric Guldbrand
+- Davíð Freyr Björnsson
+- Time spent:
+
+#%% md
+Three different resampling algorithms
+I. rejection sampling
+II. likelihood weighted sampling
+III. Gibbs sampling
+
+Apply each of the three algorithms to the following tasks.
+
+1. Compute the following probabilities, directly, without sampling. Then employ each of the three sampling algorithms to approximate the probabilities. Use 1000 samples for each method and document your results. How do the approximations compare to the true values?
