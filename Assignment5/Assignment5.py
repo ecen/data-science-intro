d3df94a5aeb8873697069058931bb5d055eb95a3
Davíð Freyr Björnsson <david24099@gmail.com>
2019-10-01 11:10:52 +0200

    Added slides

--- Assignment5/Assignment5.py
+++ Assignment5/Assignment5.py
@@ -6,11 +6,21 @@
 - Time spent:
 
 #%% md
-Three different resampling algorithms
-I. rejection sampling
-II. likelihood weighted sampling
-III. Gibbs sampling
+# ### Three different resampling algorithms:
 
-Apply each of the three algorithms to the following tasks.
+1. Rejection sampling
+2. Likelihood weighted sampling
+3. Gibbs sampling
 
-1. Compute the following probabilities, directly, without sampling. Then employ each of the three sampling algorithms to approximate the probabilities. Use 1000 samples for each method and document your results. How do the approximations compare to the true values?
+#%% md
+# ### Apply each of the three algorithms to the following tasks.
+# ### 1. Compute the following probabilities, directly, without sampling. Then employ each of the three sampling algorithms to approximate the probabilities. Use 1000 samples for each method and document your results. How do the approximations compare to the true values?
+
+# ### a. $P(D | B,C) = P(D = true) | B = true, C = true)$
+Solution: $P(D = true) | B = true, C = true) = 0.9$ (from table)
+
+# ### b. $P(X | V) = P(X = true | V = true)$
+Solution: $P(X = true | V = true) = \frac{P(X = true, V = true)}{P(V = true)}$. But:
+$$P(X = true, V = true) = \sum_{S,T,L,C \in \{true, false\}} P(X = true, V = true, S, T, L)$$
+
+# ### c. $P(C | V^c , S) = P(C = true | V = false, S = true)$
