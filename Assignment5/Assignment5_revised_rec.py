b8b6f40c44a1187a9dcda213d071587d5cfc84ae
Davíð Freyr Björnsson <david24099@gmail.com>
2019-10-02 21:01:24 +0200

    A draft of likelihood weighted sampling done.

--- Assignment5/Assignment5_revised_rec.py
+++ Assignment5/Assignment5_revised_rec.py
@@ -21,89 +21,131 @@ Solution: $P(D = true) | B = true, C = true) = 0.9$ (from table)
 
 # ### b. $P(X | V) = P(X = true | V = true)$
 Solution: $P(X = true | V = true) = \frac{P(X = true, V = true)}{P(V = true)}$. But:
-$$P(X = true, V = true) = \sum_{S,T,L,C \in \{true, false\}} P(X = true, V = true, S, T, L)$$
-
-#%%
-
-
+$$P(X = true, V = true) = $$
 
 #%% md
 
 # ### c. $P(C | V^c , S) = P(C = true | V = false, S = true)$
 
 #%%
+import pandas as pd
 import numpy as np
+from random import random
+from pandas import DataFrame as fr
 
-class Node:
-    # parents is a list of nodes
-    # table is a list of probabilities, the truth table:
-    #   no parents: 1 value
-    #   1 parent: true, false
-    #   2 parents: true true, true false, false true, false false
-    def __init__(self, name="", parents=[], table=[]):
-        self.name = name
-        self.parents = parents
-        self.table = table
+# Given dictionary (dict) and dataframe (df). Ouput the subset of the dict that contains only keys that are also in the df
+def subset_dict(dictionary, df):
+    tmp_dict = {}
+    for x in dictionary:
+        if x in df.keys():
+            tmp_dict[x] = dictionary[x]
+    return tmp_dict
 
-    def sample(self, truths=[], lies=[]):
-        for node in truths:
-            if self is node:
-                return True
+# Subset the dataframe (df), so it meets all the conditions set by the boolean dictionary (bool_dict). Output the probability associated with the subsetted df
+def get_prob(bool_dict, df):
+    # Ensure that we only set conditions on keys
+    # that exist in the dataframe
+    s = subset_dict(bool_dict, df)
+    qry = ' and '.join(["{} == {}".format(k,v) for k,v in s.items()])
+    if qry != "":
+        return df.query(qry).prob.iloc[0]
+    else:
+        return df.prob.iloc[0]
 
-        for node in lies:
-            if self is node:
-                return False
+def get_sample(graph, bool_dict={}):
+    for key in graph:
+        r = random()
+        prob = get_prob(bool_dict, graph[key])
+        bool_dict[key] = (prob > r)
+    return bool_dict
 
-        r = np.random.uniform(size = 1)
-        if len(self.parents) == 0:
-            return r < self.table[0]
-        elif len(self.parents) == 1:
-            p1 = self.parents[0].sample(truths, lies)
-            if p1:
-                return r < self.table[0]
-            else:
-                return r < self.table[1]
-        else:
-            p1 = self.parents[0].sample(truths, lies)
-            p2 = self.parents[1].sample(truths, lies)
-            if (p1 and p2):
-                return r < self.table[0]
-            elif (p1 and not p2):
-                return r < self.table[1]
-            elif (not p1 and p2):
-                return r < self.table[2]
-            else:
-                return r < self.table[3]
-
-    def toString(self):
-        s = ""
-        for p in self.parents:
-            s += p.toString() + "\n"
-        s += str(self.table)
-        return s
-
-v = Node("v", table=[0.01])
-t = Node("t", parents=[v], table=[0.05, 0.01])
-s = Node("s", table=[0.5])
-l = Node("l", parents=[s], table=[0.1, 0.01])
-b = Node("b", parents=[s], table=[0.6, 0.3])
-c = Node("c", parents=[t, l], table=[1, 1, 1, 0])
-x = Node("x", parents=[c], table=[0.98, 0.05])
-d = Node("d", parents=[c,b], table=[0.9,0.7,0.8,0.9])
-
-def calcProb(node, truths=[], lies=[], n=100000):
-    np.random.seed(42)
-    t = 0
-    for i in range(0, n):
-        if node.sample(truths, lies):
-            t += 1
-    return t / n
-
-p_d_bc = calcProb(d, [b, c])
-p_x_v = calcProb(x, [v])
-p_c_vcs = calcProb(c, [s], [v])
+# Initiate total weights
+W_all = 0
+W_strict = 0
 
 #%%
-print("P(D|B,C): ", p_d_bc)
-print("P(X|V): ", p_x_v)
-print("P(C|V-,S): ", p_c_vcs)
+# Create the dataset
+tf2 = [True, False]
+tf4_1 = [True, True, False, False]
+tf4_2 = [True, False, True, False]
+info = {
+'V': fr({'prob': [0.01]}),                                       # 1st row
+'S': fr({'prob': [0.5]}),
+'T': fr({'prob': [0.05, 0.01],'V': tf2}),                        # 2nd row
+'L': fr({'prob': [0.1, 0.01], 'S': tf2}),
+'B': fr({'prob': [0.6, 0.3], 'S': tf2}),
+'C': fr({'prob': [1.0, 1.0, 1.0, 0.0], 'T': tf4_1, 'L': tf4_2}), # 3rd row
+'X': fr({'prob': [0.98, 0.05], 'C': tf2}),                       # 4th row
+'D': fr({'prob': [0.9, 0.7, 0.8, 0.9], 'C': tf4_1, 'B': tf4_2})
+}
+
+#%%
+N = 1000
+count_all = 0
+count_strict = 0
+for i in range(N):
+    tmp = get_sample(info)
+    if tmp['B'] and tmp['C']:
+        count_all += 1
+        if tmp['D']:
+            count_strict += 1
+p_d_bc = round(count_strict/count_all, 3)
+
+#%%
+print(p_d_bc)
+
+#%%
+count_all = 0
+count_strict = 0
+for i in range(N):
+    tmp = get_sample(info)
+    if tmp['V']:
+        count_all += 1
+        if tmp['X']:
+            count_strict += 1
+p_x_v = round(count_strict/count_all, 3)
+
+#%%
+print(p_x_v)
+
+#%%
+count_all = 0
+count_strict = 0
+for i in range(N):
+    tmp = get_sample(info)
+    if (not tmp['V']) and tmp['S']:
+        count_all += 1
+        if tmp['C']:
+            count_strict += 1
+p_vc_s = round(count_strict/count_all, 3)
+#%%
+print(p_vc_s)
+
+#%%
+# For a graph
+def get_sample_mlw(graph, bool_dict={}, cond={}):
+    # Initiate the weight of the sample
+    w = 1
+    for key in graph:
+        r = random()
+        if key in cond:
+            bool_dict[key] = cond[key]
+            w *= get_prob(bool_dict, graph[key])
+        else:
+            prob = get_prob(bool_dict, graph[key])
+            bool_dict[key] = (prob > r)
+    return [bool_dict, w]
+
+#%%
+W_all = 0
+W_strict = 0
+for i in range(N):
+    tmp, w = get_sample_mlw(info, cond={'B':True, 'C':True})
+    if tmp['B'] and tmp['C']:
+        W_all += w
+        if tmp['D']:
+            W_strict += w
+p_d_bc = W_strict / W_all
+
+#%%
+print(round(W_strict / W_all , 2))
