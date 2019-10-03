d2e83aa24e5e8c42274bba12dfbcaddf3dde30b8
Davíð Freyr Björnsson <david24099@gmail.com>
2019-10-03 14:38:22 +0200

    Hyperspeeded things up.

--- Assignment5/Assignment5_revised_rec.py
+++ Assignment5/Assignment5_revised_rec.py
@@ -28,34 +28,22 @@ $$P(X = true, V = true) = $$
 # ### c. $P(C | V^c , S) = P(C = true | V = false, S = true)$
 
 #%%
-import pandas as pd
 import numpy as np
 from random import random
-from pandas import DataFrame as fr
 
-# Given dictionary (dict) and dataframe (df). Ouput the subset of the dict that contains only keys that are also in the df
-def subset_dict(dictionary, df):
-    tmp_dict = {}
-    for x in dictionary:
-        if x in df.keys():
-            tmp_dict[x] = dictionary[x]
-    return tmp_dict
-
-# Subset the dataframe (df), so it meets all the conditions set by the boolean dictionary (bool_dict). Output the probability associated with the subsetted df
-def get_prob(bool_dict, df):
-    # Ensure that we only set conditions on keys
-    # that exist in the dataframe
-    s = subset_dict(bool_dict, df)
-    qry = ' and '.join(["{} == {}".format(k,v) for k,v in s.items()])
-    if qry != "":
-        return df.query(qry).prob.iloc[0]
+def get_prob_hyperspeed(bool_dict, node_value):
+    if type(node_value) is float:
+        return node_value
     else:
-        return df.prob.iloc[0]
+        dict = node_value[1]
+        for label in node_value[0]:
+            dict = dict[bool_dict[label]]
+        return dict
 
 def get_sample(graph, bool_dict={}):
     for key in graph:
         r = random()
-        prob = get_prob(bool_dict, graph[key])
+        prob = get_prob_hyperspeed(bool_dict, graph[key])
         bool_dict[key] = (prob > r)
     return bool_dict
 
@@ -65,21 +53,14 @@ W_strict = 0
 
 #%%
 # Create the dataset
-tf2 = [True, False]
-tf4_1 = [True, True, False, False]
-tf4_2 = [True, False, True, False]
-tf8_t = [True, False, True, True, False, True, False, False]
-tf8_l = [False, True, True, False, True, True, False, False]
-tf8_b = [True, True, True, False, False, False, True, False]
-
 info = {
-'V': fr({'prob': [0.01]}),                                       # 1st row
-'S': fr({'prob': [0.5]}),
-'T': fr({'prob': [0.05, 0.01],'V': tf2}),                        # 2nd row
-'L': fr({'prob': [0.1, 0.01], 'S': tf2}),
-'B': fr({'prob': [0.6, 0.3], 'S': tf2}),
-'X': fr({'prob': [0.98, 0.98, 0.98, 0.05], 'T': tf4_1, 'L': tf4_2}),                       # 3rd row
-'D': fr({'prob': [0.9, 0.9, 0.9, 0.7, 0.7, 0.7, 0.8, 0.9], 'T': tf8_t, 'L': tf8_l, 'B': tf8_b})
+'V': 0.01,                                       # 1st row
+'S': 0.5,
+'T': (['V'], {True: 0.05, False: 0.01}),                        # 2nd row
+'L': (['S'], {True: 0.1, False: 0.01}),
+'B': (['S'], {True: 0.6, False: 0.3}),
+'X': (['T', 'L'], {True: {True: 0.98, False: 0.98}, False: {True: 0.98, False: 0.05}}), # 3rd row
+'D': (['T', 'L', 'B'], {True: {True: {True: 0.9, False: 0.7}, False: {True: 0.9, False: 0.7}}, False: {True: {True: 0.9, False: 0.7}, False: {True: 0.8, False: 0.9}}}),
 }
 
 #%% md
@@ -87,7 +68,7 @@ info = {
 # ## I. Rejection sampling
 
 #%%
-N = 1000
+N = 1000000
 count_all = 0
 count_strict = 0
 for i in range(N):
@@ -141,9 +122,9 @@ def get_sample_mlw(graph, bool_dict={}, cond={}):
         r = random()
         if key in cond:
             bool_dict[key] = cond[key]
-            w *= get_prob(bool_dict, graph[key])
+            w *= get_prob_hyperspeed(bool_dict, graph[key])
         else:
-            prob = get_prob(bool_dict, graph[key])
+            prob = get_prob_hyperspeed(bool_dict, graph[key])
             bool_dict[key] = (prob > r)
     return [bool_dict, w]
 #%%
@@ -171,7 +152,7 @@ for i in range(N):
             W_strict += w
 p_x_v = W_strict / W_all
 #%%
-print(x_v)
+print(p_x_v)
 
 #%%
 W_all = 0
@@ -184,4 +165,4 @@ for i in range(N):
             W_strict += w
 p_t_or_l_vc_s = W_strict / W_all
 #%%
-print(x_v)
+print(p_t_or_l_vc_s)
