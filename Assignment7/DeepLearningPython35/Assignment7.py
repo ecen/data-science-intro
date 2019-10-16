61c277e550da4ca408a5d1c4d86677458e65507d
ecen <ecen.cronzeton@gmail.com>
2019-10-16 13:55:35 +0200

    Save assignment 7.

--- Assignment7/DeepLearningPython35/Assignment7.py
+++ Assignment7/DeepLearningPython35/Assignment7.py
@@ -4,27 +4,30 @@
 - Davíð Freyr Björnsson
 - Eric Guldbrand
 
-#%%
 # ### 1) Using the matplotlib library, add few lines to the program to: A) also calculate the training accuracy (the number of correctly classified training samples out of 50000) after each epoch.
 # ### B) Plot the training and testing accuracies vs the epoch as two curves in the same plot.
 
-# ----------------------
-# - read the input data:
+Changed mnist_loader.load_data_wrapper to also return training_data_zip, the training data in the same format as the test data. This set is created with
+
+```
+training_data_zip = zip(training_inputs, tr_d[1])
+```
+
+#%%
+
+# read the input data
 import network
 import mnist_loader
 import seaborn as sb
-training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-training_data_zip = training_data
+training_data, validation_data, test_data, training_data_zip = mnist_loader.load_data_wrapper()
 training_data = list(training_data)
 
-# ---------------------
-# - network.py example:
+# network.py example:
 
 net = network.Network([784, 30, 10])
 
-accuracy_training = net.SGD(training_data, epochs=2, mini_batch_size=10, eta=3.0, test_data=training_data_zip)
-
-#accuracy_test = net.SGD(training_data, epochs=2, mini_batch_size=10, eta=3.0, test_data=test_data)
+accuracy_train, accuracy_test = net.SGD(training_data, epochs=30, mini_batch_size=10, eta=3.0, test_data_train=training_data_zip, test_data_test = test_data)
 
 #%%
-print(accuracy_training, "out of", len(training_data))
+sb.
+print(accuracy_train, "out of", len(training_data))
