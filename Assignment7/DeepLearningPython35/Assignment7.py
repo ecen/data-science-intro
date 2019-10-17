af84458b777b27e014722c0590abd62a18b1f8bf
ecen <ecen.cronzeton@gmail.com>
2019-10-17 11:19:59 +0200

    Created train and repeat. Almost done with 2c.

--- Assignment7/DeepLearningPython35/Assignment7.py
+++ Assignment7/DeepLearningPython35/Assignment7.py
@@ -20,6 +20,7 @@ Changes were also done to `network.py` to return both training and test accuracy
 # read the input data
 import network
 import mnist_loader
+import matplotlib.pyplot as plt
 
 def train_network(layers, epochs, batch_size, eta):
     training_data, validation_data, test_data, training_data_zip = mnist_loader.load_data_wrapper()
@@ -38,16 +39,24 @@ def plot_accuracy(accuracy_train, accuracy_test, epochs):
     df = pd.DataFrame({'epochs': range(0, epochs), 'accuracy_train': accuracy_train, 'accuracy_test': accuracy_test})
     plot = sns.scatterplot(x="epochs", y="accuracy_train", ci="sd", data=df, alpha=alpha)
     plot = sns.scatterplot(x="epochs", y="accuracy_test", ci="sd", data=df, alpha=alpha)
+    plt.legend(labels=['Training accuracy', 'Test accuracy']);
 
 def train_and_plot(layers, epochs, batch_size, eta):
     acc_train, acc_test = train_network(layers, epochs, batch_size, eta)
     plot_accuracy(acc_train, acc_test, epochs)
     return acc_train, acc_test
 
-#%%
-acc_train, acc_test = train_and_plot([784, 30, 10], 30, 10, 3.0)
+def train_and_repeat(layers, epochs, batch_size, eta, repeat):
+    acc_tests = []
+    for i in range(0, repeat):
+        acc_train, acc_test = train_network(layers, epochs, batch_size, eta)
+        acc_tests.append(acc_test[-1])
+    return acc_tests
 
-#%%)
+#%%
+acc_train, acc_test = train_and_plot([784, 30, 10], 1, 10, 3.0)
+
+#%%
 print("Training accuracy each epoch:", acc_train, acc_test)
 
 #%% md
@@ -60,27 +69,28 @@ train_and_plot([784, 30, 10], 30, 10, 3.0)
 # ## b) Change the number epochs to 10 and the number of hidden units to 100. Try different step sizes from 3 to 15. Repeat each step size 3 times. Report the testing result at the last epoch of each trial. For the learning rate with the best performance and learning rate 3, make two separate plots of performance with 30 epochs.
 
 #%%
-train_and_plot([784, 100, 10], 1, 10, 3.0)
-train_and_plot([784, 100, 10], 1, 10, 5.0)
-train_and_plot([784, 100, 10], 1, 10, 10.0)
-train_and_plot([784, 100, 10], 1, 10, 15.0)
+best_learning_rate = 3
+max_acc = -1
+for learning_rate in [3, 5, 10, 15]:
+    max_acc_new = max(train_and_repeat([784, 10, 10], 1, 10, learning_rate , 3))
+    if max_acc_new > max_acc:
+        best_learning_rate = learning_rate
+
+print("Best learning rate:", best_learning_rate)
+
+#%%
+train_and_plot([784, 100, 10], 1, 10, best_learning_rate)
+plt.figure()
+train_and_plot([784, 100, 10], 1, 10, 3)
 
 #%% md # ### c) Fix the number of epochs to 10. Create a chart of testing performance for different number of hidden units (one hidden layer and repeat 3 times) with the best learning rate by repeating part 2 above. Report the best size and best learning rate with the plot for the performance with 30 epochs.
 
 #%%
 # Hidden units: 10
-train_and_plot([784, 10, 10], 30, 10, 3.0)
-train_and_plot([784, 10, 10], 30, 10, 5.0)
-train_and_plot([784, 10, 10], 30, 10, 10.0)
-train_and_plot([784, 10, 10], 30, 10, 15.0)
+for hidden_units in [10, 20, 40, 80]:
+    train_and_repeat([784, hidden_units, 10], 10, 10, best_learning_rate, 3)
 
-# Hidden units: 20
-train_and_plot([784, 20, 10], 30, 10, 3.0)
-train_and_plot([784, 20, 10], 30, 10, 5.0)
-train_and_plot([784, 20, 10], 30, 10, 10.0)
-train_and_plot([784, 20, 10], 30, 10, 15.0)
 
-# Etc.
 
 #%% md # ### 3) Experiment with noise: a) Add few lines to the network.Network.SGD (after the line “n = len(training_data)”) to add a centered i.i.d Gaussian noise with standard deviation (std) 1 to each training data point. Use command “np.random.randn()” to create noise and note that the training_data variable is a list of tuples [x,y] of data and labels.
 
