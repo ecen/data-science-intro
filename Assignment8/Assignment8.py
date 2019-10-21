ce668780c5dd9b3862e0a375521d65938fdf6a8e
Davíð Freyr Björnsson <david24099@gmail.com>
2019-10-21 13:54:41 +0200

    Created Assignment8.py

---
+++ Assignment8/Assignment8.py
@@ -0,0 +1,13 @@
+#%% md
+# Assignemnt 8
+# ## Students:
+- Davíð Freyr Björnsson
+- Eric Guldbrand
+
+
+# ### 1) The branching factor d of a directed graph is the maximum number of children (outer degree) of a node in the graph. Suppose that the shortest path between the initial state and a goal is of length r.
+# ### a) What is the maximum number of BFS iterations required to reach the solution in terms of d and r?
+__Answer:__
+
+
+# ### b) Suppose that storing each node requires one unit of memory. Hence, storing a path with k nodes requires k units of memory. What is the maximum amount of memory required for BFS in terms of d and r?
